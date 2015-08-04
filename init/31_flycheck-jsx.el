9a49f72de32ca8d301d06bad9e4d72d2fe30d296
keicy <kei.nakamura97@gmail.com>
2015-08-04 15:45:56 +0900

    Add setting for useing .jshintrc .

--- init/31_flycheck-jsx.el
+++ init/31_flycheck-jsx.el
@@ -1,11 +1,16 @@
-(flycheck-define-checker jsxhint-checker
-  "A JSX syntax and style checker based on JSXHint."
-  :command ("jsxhint" "--config" ,(expand-file-name "~/.emacs.d/.jshintrc") source)
-  :error-patterns ((error line-start (1+ nonl) ": line " line ", col " column ", " (message) line-end))
-  :modes (web-mode))
+(flycheck-def-executable-var 'jsxhint-checker "jsxhint")
+(flycheck-define-command-checker 'jsxhint-checker
+  "A JSX syntax and style checker based on JSXHint.
+   You must insatll jsxhint with `npm insatll -g jsxhint` first"
+
+   :command `("jsxhint" "--config" ,(expand-file-name "~/.emacs.d/.jshintrc") source)
+   :error-patterns '((error line-start (1+ nonl) ": line " line ", col " column ", " (message) line-end))
+   :modes '(web-mode))
+
 (add-hook 'web-mode-hook
-          (lambda ()
-            (when (equal web-mode-content-type "jsx")
-              ;; enable flycheck
-              (flycheck-select-checker 'jsxhint-checker)
-              (flycheck-mode))))
+             (lambda ()
+               (when (equal web-mode-content-type "jsx")
+                 ;; enable flycheck
+                 (flycheck-select-checker 'jsxhint-checker)
+                 (flycheck-mode))))
+
