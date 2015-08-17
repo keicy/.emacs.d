3f9b46975f6c10f24adcce7114accba9362701d3
keicy <kei.nakamura97@gmail.com>
2015-08-17 17:41:10 +0900

    Fix ac-slime setting.

--- init/72_ac-slime.el
+++ init/72_ac-slime.el
@@ -1,4 +1,5 @@
-(bundle ac-slime
+;;;;its not desirable,,,its NOT autoload... but if it so its NOT work well...
+(bundle! ac-slime
   (with-eval-after-load-feature 'ac-slime
     (add-hook 'slime-mode-hook 'set-up-slime-ac)
     (add-hook 'slime-repl-mode-hook 'set-up-slime-ac))
