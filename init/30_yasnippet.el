bb33c247dc60d5172503aa04cbbba80321df7d27
keicy <kei.nakamura97@gmail.com>
2015-10-25 19:09:33 +0900

    Update.

--- init/30_yasnippet.el
+++ init/30_yasnippet.el
@@ -1,3 +1,7 @@
 (bundle yasnippet)
+(let ((builtin-items (expand-file-name "el-get/yasnippet/snippets" (locate-user-emacs-file emacs-version)))
+      (custom-items (expand-file-name "tools/yasnippets" user-emacs-directory)))
+  (setq yas-snippet-dirs
+        `(,builtin-items ,custom-items)))
 (yas-global-mode 1)
 (custom-set-variables '(yas-trigger-key "TAB"))
