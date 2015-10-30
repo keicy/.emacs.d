e31f50389daeac15491d17f6fe96d776e90de826
keicy <kei.nakamura97@gmail.com>
2015-10-30 13:04:43 +0900

    Update.

--- init/30_smartchr.el
+++ init/30_smartchr.el
@@ -8,13 +8,14 @@
   (progn
     (local-set-key (kbd "A") (smartchr '("(apply `!!')" "A")))
     (local-set-key (kbd "C") (smartchr '("(cond `!!')" "C")))
-    (local-set-key (kbd "D") (smartchr '("(defun `!!')" "(defvar `!!')" "(defmacro `!!')" "(defparameter `!!')" keicy:smartchr:cl:defpackage "D")))
+    (local-set-key (kbd "D") (smartchr '("(defun `!!')" "(defvar `!!')" "(defmacro `!!')" "(defparameter `!!')" "D")))
     (local-set-key (kbd "E") (smartchr '("(eql `!!')" "(equal `!!')" "(equalp `!!')" "E")))
     (local-set-key (kbd "F") (smartchr '("(funcall `!!')" "F")))
     (local-set-key (kbd "G") (smartchr '("(getf `!!')" "G")))
     (local-set-key (kbd "I") (smartchr '("(if `!!')" "I")))
     (local-set-key (kbd "J") (smartchr '("(`!!')" "\"`!!'\"" "J")))
     (local-set-key (kbd "L") (smartchr '("(let ((`!!')))" "(loop `!!')" "L")))
+    (local-set-key (kbd "P") (smartchr '(keicy:smartchr:cl:progn "Q")))
     (local-set-key (kbd "Q") (smartchr '("'" "`" "Q")))
     (local-set-key (kbd "S") (smartchr '("(setf `!!')" "S")))
     (local-set-key (kbd "U") (smartchr '("(unless `!!')" "U")))
@@ -25,15 +26,7 @@
     (local-set-key (kbd "*") (smartchr '("*`!!'*" "*")))
   ))
 
-; TODO -> replace YAsnnipet.
-	; http://fukuyama.co/yasnippet
-	; http://konbu13.hatenablog.com/entry/2014/01/12/113300
-(defun keicy:smartchr:cl:defpackage ()
+(defun keicy:smartchr:cl:progn ()
  ""
- "(in-package :cl-user)
-(defpackage :`!!'
-  (:use :cl
-        :cl-annot)
-  (:import-from :)
-(in-package :)
-(syntax:use-syntax :annot)")
+   "(progn
+      `!!')")
