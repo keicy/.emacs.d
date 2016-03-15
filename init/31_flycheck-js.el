bbe63a64bbfdc650389586e21fc51878cafb0718
keicy <kei.nakamura97@gmail.com>
2016-03-15 18:22:44 +0900

    Change JS & JSX Linter from JSHint to ESLint

--- init/31_flycheck-js.el
+++ init/31_flycheck-js.el
@@ -1,9 +1,9 @@
-(add-hook 'web-mode-hook
+(add-hook 'js2-mode-hook
           (lambda ()
             ;; flycheckに於いて優先度の高いlinter設定を無効化
             (setq-local flycheck-disabled-checkers
               (append flycheck-disabled-checkers
                 '(javascript-jshint javascript-jscs)))
             ;; modeを設定
-              (flycheck-add-mode 'javascript-eslint 'web-mode)
+              (flycheck-add-mode 'javascript-eslint 'js2-mode)
               (flycheck-mode)))
