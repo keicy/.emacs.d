49abf9f2e401db189e271afcb9219b4e2e208336
keicy <kei.nakamura97@gmail.com>
2017-01-22 15:01:02 +0900

    (#75) Fix yasnippet settings

--- init/20_yasnippet.el
+++ init/20_yasnippet.el
@@ -2,8 +2,15 @@
   (with-eval-after-load-feature 'yasnippet
 
     ; 独自定義したスニペットの置き場所
+      ; `M-x customize-group RTN yasnippet RTN` -> `Yas Snippet Dirs` で設定できる。
+      ; これはそのコピー。(ただし、ディレクトリの指定順は出力されたものとは入れ替えている。
+      ; 入れ替えることで、 `yas-new-snippet` で保存するときに表示されるディレクトリに
+      ; 先に記述したディレクトリが表示されるようになるのでUIを向上できる。
+      ; また、デフォルトの独自定義の置き場所は `~/.emacs.d/snippets`
+      ; これも同時に使いたい場合は下記のように定義すると良い。
+      ; `(add-to-list 'yas-snippet-dirs "~/.emacs.d/resources/yasnippet")`
     (setq yas-snippet-dirs
-      (append yas-snippet-dirs '("~/.emacs.d/resources/yasnippet")))
+          '("~/.emacs.d/resources/yasnippet" yas-installed-snippets-dir))
 
     ; キーバインド
     (bind-keys :map yas-minor-mode-map
