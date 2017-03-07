1c5836249dda054be3c0dd17f7abea04949a7585
keicy <kei.nakamura97@gmail.com>
2017-03-07 17:06:16 +0900

    (#101) Remove flycheck-pos-tip-mode

--- init/21_flycheck-pos-tip.el
+++ init/21_flycheck-pos-tip.el
@@ -1,3 +1,4 @@
-(bundle flycheck-pos-tip
-  (with-eval-after-load-feature 'flycheck
-    (flycheck-pos-tip-mode)))
+; 不安定なためひとまず削除
+;(bundle flycheck-pos-tip
+;  (with-eval-after-load-feature 'flycheck
+;    (flycheck-pos-tip-mode)))
