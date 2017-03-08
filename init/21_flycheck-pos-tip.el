8d15a6235f1864e98f991e8f7a3f553778770e78
keicy <kei.nakamura97@gmail.com>
2017-03-08 15:18:47 +0900

    (#103) Refactor

--- init/21_flycheck-pos-tip.el
+++ init/21_flycheck-pos-tip.el
@@ -2,4 +2,5 @@
 ;(bundle flycheck-pos-tip
 ;  (with-eval-after-load-feature 'flycheck
 ;    (flycheck-pos-tip-mode)))
-;(custom-set-variables '(flycheck-pos-tip-timeout 60))
+;(with-eval-after-load-feature 'flycheck-pos-tip
+;  (custom-set-variables '(flycheck-pos-tip-timeout 60)))
