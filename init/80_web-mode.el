75efce08ca365790a01fc87e8111c8a17d818d54
keicy <kei.nakamura97@gmail.com>
2017-07-18 01:18:17 +0900

    Add aditional settings for web-mode of Riot.js

--- init/80_web-mode.el
+++ init/80_web-mode.el
@@ -1,7 +1,11 @@
 (bundle web-mode
-  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.tag?\\'" . web-mode))
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
+
+  ; Normal HTML
+  (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+  ; Riot.js
+  (add-to-list 'auto-mode-alist '("\\.tag\\'" . web-mode))
+  (setq web-mode-engines-alist '(("riot" . "\\.tag\\'")))
 )
