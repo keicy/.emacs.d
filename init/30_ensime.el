4685e9110e297f500a9f223bb6d3bf83eddafb32
keicy <kei.nakamura97@gmail.com>
2017-10-09 23:41:40 +0900

    (#123) Remove activator, use sbt at ensime

--- init/30_ensime.el
+++ init/30_ensime.el
@@ -1,8 +1,22 @@
-;; 起動時メッセージを表示しない
-(setq ensime-startup-notification nil)
+(custom-set-variables
+  ;; 起動時メッセージを表示しない
+  '(ensime-startup-notification nil)
+  '(ensime-startup-snapshot-notification nil)
 
-;; sbtコマンド(デフォルト)の代替でactivatorコマンドを使用する
-;; これによりOS側にはActivatorのみあれば良く、sbtはインストールしないで良い
-(setq sbt:program-name "activator")
+  ;; Activator 廃止によりコメントアウト
+  ;; sbtコマンド(デフォルト)の代替でactivatorコマンドを使用する
+  ;; これによりOS側にはActivatorのみあれば良く、sbtはインストールしないで良い
+;  '(ensime-sbt-command "/home/kei/.scala_activator/bin/activator")
+;  '(sbt:program-name "activator")
+
+  ;; 自動接続有効化
+;  '(ensime-auto-connect (quote always))
+
+  ;; .ensime をPJルートに自動作成
+  '(ensime-auto-generate-config t)
+
+  ;; UIにHRLMを使う
+  '(ensime-use-helm t)
+)
 
 (bundle ensime)
--- init/emacs-custom.el
+++ init/emacs-custom.el
@@ -3,10 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ensime-auto-connect (quote always))
- '(ensime-auto-generate-config t)
- '(ensime-sbt-command "/home/kei/.scala_activator/bin/activator")
- '(ensime-use-helm t)
  '(mozc-leim-title nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
