916919fa21850c2d086af30c31d4f4134128a47f
keicy <kei.nakamura97@gmail.com>
2018-01-25 01:12:07 +0900

    Update and Refactor with introducing Emacs25

--- init/30_ensime.el
+++ init/30_ensime.el
@@ -3,14 +3,8 @@
   '(ensime-startup-notification nil)
   '(ensime-startup-snapshot-notification nil)
 
-  ;; Activator 廃止によりコメントアウト
-  ;; sbtコマンド(デフォルト)の代替でactivatorコマンドを使用する
-  ;; これによりOS側にはActivatorのみあれば良く、sbtはインストールしないで良い
-;  '(ensime-sbt-command "/home/kei/.scala_activator/bin/activator")
-;  '(sbt:program-name "activator")
-
   ;; 自動接続有効化
-;  '(ensime-auto-connect (quote always))
+  '(ensime-auto-connect (quote always))
 
   ;; .ensime をPJルートに自動作成
   '(ensime-auto-generate-config t)
--- init/emacs-custom.el
+++ init/emacs-custom.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(mozc-leim-title nil))
+ '(package-selected-packages
+   (quote
+    (company elscreen-separate-buffer-list mozc bind-key))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
