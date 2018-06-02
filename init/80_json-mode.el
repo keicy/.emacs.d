db417b45bd4321cc837b4d694f7e22213d3eb76a
keicy <kei.nakamura97@gmail.com>
2018-06-03 00:37:29 +0900

    Add json-mode

---
+++ init/80_json-mode.el
@@ -0,0 +1,10 @@
+(bundle json-mode
+  (add-to-list 'auto-mode-alist '("\\.json\\'" . json-mode)))
+
+(add-hook 'json-mode-hook
+          (lambda ()
+            (setq js-indent-level 2)
+            (flycheck-mode)
+            (setq flycheck-checker 'json-jsonlint)
+            (flycheck-add-mode 'json-jsonlint 'json-mode)
+            ))
