cb25659a9ac8b925374a0a155a5917a52c0228fa
keicy <kei.nakamura97@gmail.com>
2018-06-10 15:55:24 +0900

    Refactor

--- init/emacs-custom.el
+++ init/emacs-custom.el
@@ -6,20 +6,22 @@
  '(package-selected-packages
    (quote
     (company elscreen-separate-buffer-list mozc bind-key))))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(mode-line ((t (:background "orange" :foreground "black" :box (:line-width -1 :style released-button)))))
-  '(rainbow-delimiters-depth-1-face ((t (:foreground "dark glay"))))
-  '(rainbow-delimiters-depth-2-face ((t (:foreground "magenta"))))
-  '(rainbow-delimiters-depth-3-face ((t (:foreground "deep sky blue"))))
-  '(rainbow-delimiters-depth-4-face ((t (:foreground "dark orange"))))
-  '(rainbow-delimiters-depth-5-face ((t (:foreground "dark violet"))))
-  '(rainbow-delimiters-depth-6-face ((t (:foreground "dark glay"))))
-  '(rainbow-delimiters-depth-7-face ((t (:foreground "magenta"))))
-  '(rainbow-delimiters-depth-8-face ((t (:foreground "blue"))))
-  '(rainbow-delimiters-depth-9-face ((t (:foreground "dark orange"))))
-  '(rainbow-delimiters-unmatched-face ((t (:foreground "red"))))
+
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "dark glay"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "magenta"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "deep sky blue"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "dark orange"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "dark violet"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "dark glay"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "magenta"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "blue"))))
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "dark orange"))))
+ '(rainbow-delimiters-unmatched-face ((t (:foreground "red"))))
 )
